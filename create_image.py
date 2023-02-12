0dd5668a7663c2775d7099c3d1a0b2d80ca9ad7c
Nico <nicomenzii@gmail.com>
2023-02-12 17:43:53 +0100

    add a bunch of stuff

--- create_image.py
+++ create_image.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageDraw, ImageFont
 from get_data import get_tx
 
-def generate_image(project_name, count_buy, count_sell, count_mint, profit, discord_id):
+def generate_image(project_name, count_buy, count_sell, count_mint, avg_buy_price, avg_sell_price, profit, discord_id):
 
 
     # Display project name, buy count, sell count, and profit in image
@@ -14,6 +14,8 @@ def generate_image(project_name, count_buy, count_sell, count_mint, profit, disc
     d.text((10,70), "NFT Sell: " + str(count_sell),  fill=(0, 0, 0))
     d.text((10,90), "Still in wallet: " + str(count_buy + count_mint - count_sell),  fill=(0, 0, 0))
     d.text((10,110), "Profit: " + str(profit) + " ETH", fill=(0, 0, 0))
+    d.text((10,130), "Average Buy Price: " + str(avg_buy_price) + " ETH", fill=(0, 0, 0))
+    d.text((10,150), "Average Sell Price: " + str(avg_sell_price) + " ETH", fill=(0, 0, 0))
     img.save(f'pil_text_font{discord_id}.png')
 
 def generate_image_time(count_buy, count_sell, profit, discord_id, timestamp):
